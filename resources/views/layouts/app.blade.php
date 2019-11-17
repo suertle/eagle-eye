5e27c3cfa294473f30f0e6c5121c338c3bcfadb4
Teeraphat <teeraphat.btt@gmail.com>
2019-11-17 15:57:34 +0700

    update title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Eagle Eye</title>
+
+    <link rel="shortcut icon" href="/favicon.ico"/>
 
     <!-- Styles -->
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
